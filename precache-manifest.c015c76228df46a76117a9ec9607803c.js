9f447687e3fe3b2a702c65abdd6aa49673cad887
Mike <klimoff.pro@ya.ru>
2020-08-13 17:54:40 +0300

    Add files via upload

---
+++ precache-manifest.c015c76228df46a76117a9ec9607803c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bfd6362800e6e39e3801f288404ddb9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b077c08b3d8d40ce604f",
+    "url": "/static/css/main.7ff0a3b5.chunk.css"
+  },
+  {
+    "revision": "9a981f050e66a2d74006",
+    "url": "/static/js/2.aff3f410.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.aff3f410.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b077c08b3d8d40ce604f",
+    "url": "/static/js/main.aba9f8b0.chunk.js"
+  },
+  {
+    "revision": "78af287607277a92e755",
+    "url": "/static/js/runtime-main.28b8ab72.js"
+  }
+]);
